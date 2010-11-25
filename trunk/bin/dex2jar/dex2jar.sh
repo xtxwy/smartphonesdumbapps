830c63fad4d828927598a5b66b1cc7e851ad147e
dancornell <dancornell@b94a1244-6b50-bac4-e592-b98c52cb6a57>
2010-11-25 14:51:00 +0000

    -Added scripted dex2jar to analyze_android.pl -Included findbugs 1.3.9 in the distribution -Added scripted findbugs to analyze_android.pl

--- trunk/bin/dex2jar/dex2jar.sh
+++ trunk/bin/dex2jar/dex2jar.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+IN_DIR=`dirname $0`
+
 _classpath="."
-for k in lib/*.jar
+for k in $IN_DIR/lib/*.jar
 do
  _classpath="${_classpath}:${k}"
 done
+
+#DanCo - Added for debugging
+echo "Dex2jar classpath: " ${_classpath}
+
 java  -classpath "${_classpath}" "pxb.android.dex2jar.v3.Main" $1 $2 $3 $4 $5 $6
